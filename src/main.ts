1038d84d5b4d938ef5779c924a0966fa65d8de29
WahyuGiri04 <giartobudi626@gmail.com>
2025-10-18 18:07:02 +0700

    add depedency

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,23 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
+import '@/assets/tailwind.css'
+import '@/assets/styles.scss'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      darkModeSelector: '.app-dark',
+    },
+  },
+})
 
 app.mount('#app')
---
+++ src/views/LandingPage.vue
@@ -0,0 +1,5 @@
+<script lang="ts"></script>
+
+<template>
+  <div class="text-black p-10 text-center w-full bg-red-600 rounded-full">Landing Page</div>
+</template>
