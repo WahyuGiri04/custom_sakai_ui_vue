268df21debd16e790029f2b63f025a1f1ab7f0aa
WahyuGiri04 <giartobudi626@gmail.com>
2025-10-23 19:35:00 +0700

    add function hide menu

--- src/layout/AppLayout.vue
+++ src/layout/AppLayout.vue
@@ -1,11 +1,73 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import { computed, ref, watch } from 'vue'
+import { useLayout } from './composables/layout'
 import AppTopbar from './AppTopbar.vue'
 import AppSidebar from './AppSidebar.vue'
+
+const { layoutConfig, layoutState, isSidebarActive } = useLayout()
+
+const outsideClickListener = ref<((event: MouseEvent) => void) | null>(null)
+
+watch(isSidebarActive, (newVal) => {
+  if (newVal) {
+    bindOutsideClickListener()
+  } else {
+    unbindOutsideClickListener()
+  }
+})
+
+const containerClass = computed(() => {
+  return {
+    'layout-overlay': layoutConfig.menuMode === 'overlay',
+    'layout-static': layoutConfig.menuMode === 'static',
+    'layout-static-inactive':
+      layoutState.staticMenuDesktopInactive && layoutConfig.menuMode === 'static',
+    'layout-overlay-active': layoutState.overlayMenuActive,
+    'layout-mobile-active': layoutState.staticMenuMobileActive,
+  }
+})
+
+function bindOutsideClickListener() {
+  if (!outsideClickListener.value) {
+    outsideClickListener.value = (event) => {
+      if (isOutsideClicked(event)) {
+        layoutState.overlayMenuActive = false
+        layoutState.staticMenuMobileActive = false
+        layoutState.menuHoverActive = false
+      }
+    }
+    document.addEventListener('click', outsideClickListener.value)
+  }
+}
+
+function unbindOutsideClickListener() {
+  if (outsideClickListener.value) {
+    document.removeEventListener('click', outsideClickListener.value)
+    outsideClickListener.value = null
+  }
+}
+
+function isOutsideClicked(event: MouseEvent) {
+  const sidebarEl = document.querySelector('.layout-sidebar')
+  const topbarEl = document.querySelector('.layout-menu-button')
+  const target = event.target as Node | null
+
+  if (!sidebarEl || !topbarEl || !target) {
+    return false
+  }
+
+  return !(
+    sidebarEl.isSameNode(target) ||
+    sidebarEl.contains(target) ||
+    topbarEl.isSameNode(target) ||
+    topbarEl.contains(target)
+  )
+}
 </script>
 
 <template>
-  <div class="layout-wrapper">
+  <div class="layout-wrapper" :class="containerClass">
     <AppTopbar></AppTopbar>
     <AppSidebar></AppSidebar>
     <div class="layout-main-container">
@@ -13,5 +75,6 @@ import AppSidebar from './AppSidebar.vue'
         <RouterView />
       </div>
     </div>
+    <div class="layout-mask animate-fadein"></div>
   </div>
 </template>
